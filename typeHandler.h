b864a50384d74158da8bc0c02b46dd1e75a35240
cloudstudyJ <rgwh0404@gmail.com>
2023-11-16 14:36:09 +0900

    Added enableIF (SFINAE)

--- typeHandler.h
+++ typeHandler.h
@@ -4,9 +4,15 @@ namespace typeHandlingBase {
     template <typename T> struct removeReference      { using type = T; };
     template <typename T> struct removeReference<T&>  { using type = T; };
     template <typename T> struct removeReference<T&&> { using type = T; };
+
+    template <bool, typename = void> struct enableIF          { };
+	template <typename T>            struct enableIF<true, T> { using type = T; };
 }
 
 template <typename T> using removeReference = typename typeHandlingBase::removeReference<T>::type;
+
+template <bool Condition, typename T = void>
+using enableIF = typename typeHandlingBase::enableIF<Condition, T>::type;
 
 template <typename, typename> inline constexpr bool isSame       = false;
 template <typename T>         inline constexpr bool isSame<T, T> = true;
